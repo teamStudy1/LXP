13eb0483655b4e4e502953d6c88ef2e8d3270ecd
minyong96 <aldud159357@gmail.com>
2025-10-16 17:00:55 +0900

    feat: set course

--- src/main/java/com/lxp/domain/course/CourseDetail.java
+++ src/main/java/com/lxp/domain/course/CourseDetail.java
@@ -1,10 +1,12 @@
 package com.lxp.domain.course;
 
 import java.time.Instant;
-import java.util.Objects;
-//VO 값 객체
-record CourseDetail(String sb, Long courseId, String content, String contentDetail, Instant createdAt, Instant updatedAt)
 
-{
-
-}
+// VO 값 객체
+record CourseDetail(
+        String sb,
+        Long courseId,
+        String content,
+        String contentDetail,
+        Instant createdAt,
+        Instant updatedAt) {}
